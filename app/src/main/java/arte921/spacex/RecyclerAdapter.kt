41be9c79561e11521ba98412aaabc8e8e84b91f3
arte921 <ardjanve@gmail.com>
2020-03-11 21:41:45 +0100

    w

--- app/src/main/java/arte921/spacex/RecyclerAdapter.kt
+++ app/src/main/java/arte921/spacex/RecyclerAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.launchcard.view.*
 import org.json.JSONObject
 import java.lang.Math.random
+import java.util.*
 
 class RecyclerAdapter() : RecyclerView.Adapter<RecyclerAdapter.MainViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.MainViewHolder = MainViewHolder(
@@ -29,25 +30,28 @@ class RecyclerAdapter() : RecyclerView.Adapter<RecyclerAdapter.MainViewHolder>()
 
             SUCCESSFUL -> {
                 val jo = json.getJSONObject(position)
-                holder.view.tva.text = jo.getJSONObject("launch_site").get("site_name_long").toString()
+                holder.view.missionname.text = "Mission: " + jo.get("mission_name").toString()
+                holder.view.datetime.text = "Launches at: " + java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date(jo.get("launch_date_unix").toString().toLong() * 1000)).toString()
+                holder.view.rocketname.text = "Spacecraft: " + jo.getJSONObject("rocket").get("rocket_name").toString()
+                holder.view.launchlocation.text = jo.getJSONObject("launch_site").get("site_name_long").toString()
             }
 
-            ISLOADING -> holder.view.tva.text = context.getString(R.string.loading)
+            ISLOADING -> holder.view.datetime.text = context.getString(R.string.loading)
 
             HASFAILED -> {
                 when(position) {
-                    0 -> holder.view.tva.text = context.getString(R.string.failed1)
-                    1 -> holder.view.tva.text = context.getString(R.string.failed2)
-                    else -> holder.view.tva.text = ""
+                    0 -> holder.view.datetime.text = context.getString(R.string.failed1)
+                    1 -> holder.view.datetime.text = context.getString(R.string.failed2)
+                    else -> holder.view.datetime.text = ""
                 }
             }
 
             TESTING -> {
                 when(position) {
-                    0 -> holder.view.tva.text = context.getString(R.string.test)
-                    1 -> holder.view.tva.text = random().toString()
-                    2 -> holder.view.tva.text = utilTestString
-                    else -> holder.view.tva.text = ""
+                    0 -> holder.view.datetime.text = context.getString(R.string.test)
+                    1 -> holder.view.datetime.text = random().toString()
+                    2 -> holder.view.datetime.text = utilTestString
+                    else -> holder.view.datetime.text = ""
                 }
             }
 
