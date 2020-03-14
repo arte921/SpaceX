d32a725e45710e56058aa9e22ab2257d52e8343a
arte921 <ardjanve@gmail.com>
2020-03-14 14:09:48 +0100

    release 1.0 version

--- app/src/main/java/arte921/spacex/PayloadsAdapter.kt
+++ app/src/main/java/arte921/spacex/PayloadsAdapter.kt
@@ -3,9 +3,11 @@ package arte921.spacex
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.payloadslayout.view.*
 import org.json.JSONArray
+import org.json.JSONObject
 import java.lang.Math.random
 
 class PayloadsAdapter(private val pjo: JSONArray): RecyclerView.Adapter<PayloadsAdapter.MainViewHolder>() {
@@ -21,21 +23,36 @@ class PayloadsAdapter(private val pjo: JSONArray): RecyclerView.Adapter<Payloads
         holder.view.payloadnumber.text = (position + 1).toString()
         holder.view.payloadname.text = jo.get("payload_id").toString()
         holder.view.payloadtype.text = jo.get("payload_type").toString()
-        holder.view.manufacturer.text = jo.get("manufacturer").toString()
-        holder.view.nationality.text = jo.get("nationality").toString()
-        holder.view.payloadmass.text = "${jo.get("payload_mass_kg")} kg"
 
-        holder.view.orbittype.text = jo.getJSONObject("orbit_params").get("reference_system").toString()
-        holder.view.regime.text = jo.getJSONObject("orbit_params").get("regime").toString()
+
+        holder.view.nationality.text = jo.get("nationality").toString()
+        holder.view.payloadmass.text = jo.get("payload_mass_kg").toString()
+
+        if(jo.get("manufacturer") != JSONObject.NULL) holder.view.manufacturer.text = jo.get("manufacturer").toString() else {
+            holder.view.manufacturer.visibility = View.GONE
+            holder.view.mansign.visibility = View.GONE
+        }
+
+        if(jo.get("nationality") != JSONObject.NULL) holder.view.manufacturer.text = jo.get("nationality").toString() else {
+            holder.view.nationality.visibility = View.GONE
+            holder.view.natsign.visibility = View.GONE
+        }
+
+        if(jo.get("payload_mass_kg") != JSONObject.NULL) holder.view.manufacturer.text = jo.get("payload_mass_kg").toString() else {
+            holder.view.payloadmass.visibility = View.GONE
+            holder.view.payloadmasssign.visibility = View.GONE
+        }
 
         var customerstring = ""
         for(i in 0 until jo.getJSONArray("customers").length()){
             if(i > 0) customerstring += ", "
             customerstring += jo.getJSONArray("customers").get(i)
         }
-        holder.view.customers.text = customerstring
+        if(customerstring != "") holder.view.customers.text = customerstring else{
+            holder.view.customers.visibility = View.GONE
+            holder.view.customerssign.visibility = View.GONE
+        }
 
     }
-
 }
 
